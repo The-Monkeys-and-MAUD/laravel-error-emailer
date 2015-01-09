2303f72645ddb14754143b389bea2f7b4deff7d9
Hans VN <sem@brandworks.be>
2015-01-09 17:53:00 +0100

    ignoring errors from crawler bots

--- src/Themonkeys/ErrorEmailer/ErrorEmailer.php
+++ src/Themonkeys/ErrorEmailer/ErrorEmailer.php
@@ -10,38 +10,54 @@ use Illuminate\Support\Facades\View;
 class ErrorEmailer {
     public function sendException($exception)
     {
-        $recipients = Config::get("error-emailer::to");
-        if (isset($recipients['address'])) {
-            // this is a single recipient
-            if ($recipients['address']) {
-                $recipients = array($recipients);
-            } else {
-                $recipients = array();
-            }
-        }
-
-        if (sizeof($recipients) > 0) {
-            if ($exception instanceof FlattenException) {
-                $flattened = $exception;
-            } else {
-                $flattened = FlattenException::create($exception);
-            }
-            $handler = new ExceptionHandler();
-            $content = $handler->getContent($flattened);
-
-            $model = array(
-                'trace' => $content,
-                'exception' => $exception,
-                'flattened' => $flattened
-            );
-            Mail::send('error-emailer::error', $model, function($message) use ($model, $recipients) {
-                $subject = View::make('error-emailer::subject', $model)->render();
-
-                $message->subject($subject);
-                foreach ($recipients as $to) {
-                    $message->to($to['address'], $to['name']);
+        if(!errorFromBot()) {
+            $recipients = Config::get("error-emailer::to");
+            if (isset($recipients['address'])) {
+                // this is a single recipient
+                if ($recipients['address']) {
+                    $recipients = array($recipients);
+                } else {
+                    $recipients = array();
                 }
-            });
+            }
+
+            if (sizeof($recipients) > 0) {
+                if ($exception instanceof FlattenException) {
+                    $flattened = $exception;
+                } else {
+                    $flattened = FlattenException::create($exception);
+                }
+                $handler = new ExceptionHandler();
+                $content = $handler->getContent($flattened);
+
+                $model = array(
+                    'trace' => $content,
+                    'exception' => $exception,
+                    'flattened' => $flattened
+                );
+                Mail::send('error-emailer::error', $model, function($message) use ($model, $recipients) {
+                    $subject = View::make('error-emailer::subject', $model)->render();
+
+                    $message->subject($subject);
+                    foreach ($recipients as $to) {
+                        $message->to($to['address'], $to['name']);
+                    }
+                });
+            }
         }
+    }
+
+    protected static function errorFromBot() {
+        $ignoredBots = Config::get("error-emailer::ignoredBots");
+        $serverUserAgent = array_key_exists('HTTP_USER_AGENT', $_SERVER) ? $_SERVER['HTTP_USER_AGENT'] : null;
+        $serverFrom = array_key_exists('HTTP_FROM', $_SERVER) ? $_SERVER['HTTP_FROM'] : null;
+        if(is_array($ignoredBots)) {
+            foreach ($ignoredBots as $bot) {
+                if( ($serverUserAgent && strpos(strtolower($serverUserAgent), $bot) !== false) || ($serverFrom && strpos(strtolower($serverFrom), $bot) !== false) )
+                    return true;
+            }
+        }
+
+        return false;
     }
 } 
